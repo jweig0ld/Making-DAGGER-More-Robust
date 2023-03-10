8fb750fbff12a4bf2e4dbd30de3be1d5fd960c84
jweig0ld <jamie.weigold.19@ucl.ac.uk>
2023-03-10 14:28:01 +0000

    dagger trains on LunarLander

--- dagger.py
+++ dagger.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import tempfile
+import os
 
 from huggingface_sb3 import load_from_hub
 from stable_baselines3 import PPO
@@ -9,13 +10,16 @@ from stable_baselines3.common.env_util import make_vec_env
 
 from imitation.algorithms import bc
 from imitation.algorithms.dagger import SimpleDAggerTrainer
+from imitation.util.logger import make_output_format, configure
 
 
 ENV = 'Lunar Lander' # 'Lunar Lander' or 'Bipedal Walker'
-TRAIN = False 
+TRAIN = True 
 N = 10 # Number of initial expert demonstrations 
 T = 1000 # Number of timesteps per trajectory
 rng = np.random.default_rng(0)
+CHECKPOINT_RATE = 1000
+LOGDIR = 'logs'
 
 
 if ENV == 'Lunar Lander':
@@ -27,20 +31,32 @@ elif ENV == 'Bipedal Walker':
     checkpoint = load_from_hub()
 
 if TRAIN:
-    with tempfile.TemporaryDirectory(prefix="dagger_example_") as tmpdir:
-        bc_trainer = bc.BC(
-            observation_space=env.observation_space,
-            action_space=env.action_space,
-            rng=rng,
-        )
+    version = 9
+    path = "dagger_example_" + str(version)
+    # logger = make_output_format('csv', LOGDIR)
+    logger = configure('logs' + str(version), ['csv'])
 
-        dagger_trainer = SimpleDAggerTrainer(
-            venv=env,
-            scratch_dir=tmpdir,
-            expert_policy=expert,
-            bc_trainer=bc_trainer,
-            rng=rng,
-        )
+    try:
+      os.mkdir(path)
+    except OSError as error:
+      print(error)
 
-        dagger_trainer.train(2000)
-    
+    bc_trainer = bc.BC(
+        observation_space=env.observation_space,
+        action_space=env.action_space,
+        rng=rng,
+        custom_logger=logger,
+    )
+
+    dagger_trainer = SimpleDAggerTrainer(
+        venv=env,
+        scratch_dir=path,
+        expert_policy=expert,
+        bc_trainer=bc_trainer,
+        rng=rng
+    )
+
+    print("Commencing Training...")
+    dagger_trainer.train(5000, bc_train_kwargs={'log_interval': 50})
+    # dagger_trainer.save_trainer()
+    dagger_trainer.save_policy(path+"_policy.pt")
