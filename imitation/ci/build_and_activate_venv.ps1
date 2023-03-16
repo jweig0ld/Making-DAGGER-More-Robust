76e31b180a2ef325ce68c0e14cf2b57e5f46a971
jweig0ld <jamie.weigold.19@ucl.ac.uk>
2023-03-16 15:20:09 +0000

    Added Imitation Code

---
+++ imitation/ci/build_and_activate_venv.ps1
@@ -0,0 +1,14 @@
+Param(
+   $venv
+)
+$ErrorActionPreference = "Stop"  # exit immediately on any error
+
+If ($venv -eq $null) {
+   $venv = "venv"
+}
+
+virtualenv -p python3.8 $venv
+& $venv\Scripts\activate
+# Note: We need to install setuptools==66.1.1 to allow installing gym==0.21.0.
+python -m pip install --upgrade pip setuptools==66.1.1
+pip install ".[docs,parallel,test]"
